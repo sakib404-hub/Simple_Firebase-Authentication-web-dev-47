c1894a9bb43cbfe47ff4093a2958c72554bbd1b5
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-10-17 20:34:42 +0600

    set the basic things like daisy khala, tailwind and react router

---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <div className='p-4 bg-base-200 shadow-md'>
+                <h1 className='text-2xl font-semibold text-center'>Simple Firebase Authentication</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Root;
